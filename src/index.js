c4ede925960d6bcb8cbfd46a5e459882cf232332
Vladyslav Kostenko <vlad.k@integrio.net>
2021-12-23 12:23:22 +0200

    lesson21

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import timer from "./modules/timer";
 import menu from "./modules/menu";
 import modal from "./modules/modal"
-import scrollSmoth from "./modules/scrollSmooth"
+import scrollSmoth from "./modules/scrollSmooth";
+import validate from "./modules/validate"
 
 timer('23 december 2021')
 menu()
 modal()
 scrollSmoth()
+validate()
 
 
 
---
+++ src/modules/validate.js
@@ -0,0 +1,59 @@
+const validate = () => {
+    const calcBlock = document.querySelector('.calc-block')
+    const inputs = calcBlock.querySelectorAll('input[type=text]');
+    const nameUserOne = document.querySelector('#form1-name')
+    const nameUserTwo = document.querySelector('#form2-name')
+    const nameUserThree = document.querySelector('#form3-name')
+    const message = document.querySelector('.mess');  
+    const emailsInputs = document.querySelectorAll('input[type=email]')
+    const phoneInputs = document.querySelectorAll('input[type=tel]');
+    
+
+    calcBlock.addEventListener('input', (e) => {
+        if (e.target.type === 'text') {
+            e.target.value = e.target.value.replace(/[^\d]/g, '')
+           
+        }
+    })
+    emailsInputs.forEach(email => {
+        email.addEventListener('input', () => {
+        email.value = email.value.replace(/[^a-zA-Z\@\-\_\.\!\~\*\'/]/g,'')
+        })
+    })
+  
+    phoneInputs.forEach(phone => {
+        phone.addEventListener('input', () => {
+            phone.value = phone.value.replace(/[^0-9\(\)\-]/g,'')
+        })
+    })
+        nameUserOne.addEventListener('input', () => {
+
+            let namePlaceholderOne = nameUserOne.getAttribute('placeholder')
+            if(namePlaceholderOne === 'Ваше имя') {
+                nameUserOne.value = nameUserOne.value.replace(/[^а-яА-Я\s\-]/g, '')
+            }
+        })
+        nameUserTwo.addEventListener('input', () => {
+
+            let namePlaceholderOne = nameUserOne.getAttribute('placeholder')
+            if(namePlaceholderOne === 'Ваше имя') {
+                nameUserOne.value = nameUserOne.value.replace(/[^а-яА-Я\s\-]/g, '')
+            }
+        })
+        nameUserThree.addEventListener('input', () => {
+
+            let namePlaceholderOne = nameUserOne.getAttribute('placeholder')
+            if(namePlaceholderOne === 'Ваше имя') {
+                nameUserOne.value = nameUserOne.value.replace(/[^а-яА-Я\s\-]/g, '')
+            }
+        })
+        message.addEventListener('input', () => {
+            
+            let namePlaceholderOne = message.getAttribute('placeholder')
+            if(namePlaceholderOne === 'Ваше сообщение') {
+                message.value = message.value.replace(/[^а-яА-Я\s\-]/g, '')
+            }
+        })
+
+}
+export default validate
